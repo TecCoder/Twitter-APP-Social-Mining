ae7248aab2ae6aeda5ab5c958b6a2de858368a2e
TecCoder <davidalvaromartinez@hotmail.com>
2020-06-22 19:32:34 +0200

    Add files via upload

---
+++ Funciones.R
@@ -0,0 +1,232 @@
+authentication <- function (){
+api_key <- "leG8otLsNt4344LHPvBJZPGX7"
+api_secret_key <- "12bvZcWDp8RaooR3Gypo39VCjJS7ZSzXlLeWhiikVfB5pwARHL"
+access_token <- "239048889-sKudn7Y4ZKxRuY1GTymKsueWLWnjcJM8UpuhXEQk"
+access_token_secret <- "DfXPH6Aw1ngJdK5U0tmMogTNotRSZapr6KqTiOqHHDsmZ"
+
+## authenticate via web browser
+token <- create_token(
+  app = "sandboxMGTPN",
+  consumer_key = api_key,
+  consumer_secret = api_secret_key,
+  access_token =access_token,
+  access_secret=access_token_secret)
+
+return(token)
+}
+
+
+TopicModeler <- function(df,n) {
+  
+  df <- clean_text(df)
+  
+  df <- df %>% select(status_id,text) %>%
+    unnest_tokens(word,text)
+  
+  df <- df %>% anti_join(custom_stop_words)
+  
+  dfdfm <-df %>% count(status_id,word,sort=TRUE) %>%
+    
+    cast_dfm(status_id, word, n)
+  
+  topicmodel <-  stm(dfdfm, K = n, 
+                     verbose = FALSE, init.type = "Spectral")
+  
+  tm_beta <- tidy(topicmodel)
+  
+  
+  tm_beta %>%
+    group_by(topic) %>%
+    top_n(10, beta) %>%
+    ungroup() %>%
+    mutate(topic = paste0("Topic ", topic),
+           term = reorder_within(term, beta, topic)) %>%
+    ggplot(aes(term, beta, fill = as.factor(topic))) +
+    geom_col(alpha = 0.8, show.legend = FALSE) +
+    facet_wrap(~ topic, scales = "free_y") +
+    coord_flip() +
+    scale_x_reordered() +
+    labs(x = NULL, y = expression(beta),
+         title = "Highest word probabilities for each topic",
+         subtitle = "Different words are associated with different topics")
+  
+}
+
+TopicModeler2 <- function(df,n,z) {
+  
+  df <- clean_text(df)
+  
+  if(z==1) {
+    df <- df %>%
+      select(c(status_id,text)) %>%      #Si no hacemos select podemos destriparlo simplemente para tener todo en una palabra por fila y tweet
+      unnest_tokens(word, text) ## Si la condición se cumple (TRUE)
+    df <- df %>%
+      anti_join(custom_stop_words) %>%
+      filter(!word=="t.co" %in% word)
+    
+  } else {
+    df  <- df %>%
+      select(status_id,text) %>%
+      unnest_tokens(paired_words, text, token = "ngrams", n = 2) ## Si la condición no se cumple (FALSE)
+    
+    df <- df %>%
+      separate(paired_words, c("word1", "word2"), sep = " ")
+    df <- df %>%
+      filter(!word1 %in% custom_stop_words$word) %>%
+      filter(!word2 %in% custom_stop_words$word)
+    df <- df %>%
+      unite(word, word1, word2, sep = " ")
+  
+  }
+  
+  dfdfm <-df %>% count(status_id,word,sort=TRUE) %>%
+    
+    cast_dfm(status_id, word, n)
+  
+  topicmodel <-  stm(dfdfm, K = n, 
+                     verbose = FALSE, init.type = "Spectral")
+  
+  tm_beta <- tidy(topicmodel)
+  
+  
+  tm_beta %>%
+    group_by(topic) %>%
+    top_n(10, beta) %>%
+    ungroup() %>%
+    mutate(topic = paste0("Topic ", topic),
+           term = reorder_within(term, beta, topic)) %>%
+    ggplot(aes(term, beta, fill = as.factor(topic))) +
+    geom_col(alpha = 0.8, show.legend = FALSE) +
+    facet_wrap(~ topic, scales = "free_y") +
+    coord_flip() +
+    scale_x_reordered() +
+    labs(x = NULL, y = expression(beta),
+         title = "Highest word probabilities for each topic",
+         subtitle = "Different words are associated with different topics")
+  
+}
+
+sentimentplot <- function (df){
+  
+  
+  df <- clean_text(df)
+  df2 <- sentiment_by(df$text)
+  df$score <- df2$ave_sentiment
+  df <- df %>% mutate(Tipo = ifelse(score > 0, "Positivo", "Negativo"))
+  
+  #fig <- plot_ly(data=count(df,Tipo), labels = ~Tipo, values = ~n, type = 'pie')
+  
+  dfagre <- aggregate(df["score"],by=df["created_at"],mean)
+  dfagre <- dfagre %>% mutate(MediaR = rollmean(score, k = 21, align = "right", na.pad = TRUE))
+  
+  fig <- plot_ly(data=dfagre, x=~created_at,y= ~score,type = 'scatter', mode = 'lines')  %>% 
+    add_trace(y = ~MediaR, name = 'Media Movil',mode = 'lines')
+  
+  return(fig)
+  
+}
+
+
+
+funwordcloud2 <- function(df,n,z){
+  
+  #df <- df %>% plain_tweets() # Esta función es útil, si solo trabajas con tweets en inglés
+  #df$text <- gsub("[^a-zA-Z0-9]","",df$text) #NADA
+  #df$text  <- gsub("[^\x01-\x7F]", "", df$text) # Esta función elimina todos los simbolos ASCII excepto los especificados
+  df$text  <- gsub("http.*","",  df$text) # Remueve cualquier tipo de enlace
+  df$text <- sapply(df$text,function(row) iconv(row, "UTF-8", "ASCII//TRANSLIT", sub="")) #Elimina emoticonos y acéntos transformando todo a ASCII. 
+  #df$text = gsub("http\\w+", "", df$text)
+  
+  #df$text <- gsub("https://t.co/[A-Za-z\\d]+|&amp;","", df$text)
+  #df$text <- gsub("//t.co/.*","" ,df$text)
+  #df$text= gsub("[[:digit:]]", "", df$text)
+  
+  df$text= gsub("@\\w+", "", df$text) # Remueve las menciones dentro de cada tweet
+  #df$text  <- gsub("[[:punct:]]", "", df$text) #Remueve estos simbolos "a1~!@#$%^&*(){}_+:\"<>?,./;'[]-="
+  
+  if(n==1) {
+    df <- df %>%
+      select(c(screen_name,text)) %>%      #Si no hacemos select podemos destriparlo simplemente para tener todo en una palabra por fila y tweet
+      unnest_tokens(word, text) ## Si la condición se cumple (TRUE)
+    df <- df %>%
+      anti_join(custom_stop_words) %>%
+      filter(!word=="t.co" %in% word)
+    
+  } else {
+    df  <- df %>%
+      select(screen_name,text) %>%
+      unnest_tokens(paired_words, text, token = "ngrams", n = 2) ## Si la condición no se cumple (FALSE)
+    
+    df <- df %>%
+      separate(paired_words, c("word1", "word2"), sep = " ")
+    df <- df %>%
+      filter(!word1 %in% custom_stop_words$word) %>%
+      filter(!word2 %in% custom_stop_words$word)
+    df <- df %>%
+      unite(word, word1, word2, sep = " ")
+    
+    
+  }
+  
+  df <- df %>% 
+    group_by(screen_name) %>%
+    count(word, sort=TRUE)
+  
+  dfdtm <- df %>% cast_dtm(screen_name, word, n)
+  
+  dfmatrix <- as.matrix(dfdtm) 
+  words <- sort(colSums(dfmatrix),decreasing=TRUE) 
+  df <- data.frame(word = names(words),freq=words)
+  
+  set.seed(1234) # for reproducibility 
+  
+  
+  
+  wordcloud2(df, size=z)
+  
+  
+  
+  
+}
+
+clean_text <- function (df) {
+  
+  #removes emoticons
+  #df$text <- sapply(df$text,function(row) iconv(row, "latin1", "ASCII", sub="")) #Elimina emoticonos
+  #df$text = gsub("(f|ht)tp(s?)://(.*)[.][a-z]+", "", df$text) 
+  #df <- df %>% plain_tweets()
+  
+  df$text <- sapply(df$text,function(row) iconv(row, "UTF-8", "ASCII//TRANSLIT", sub="")) #Elimina emoticonos y acentos
+  df$text  <- gsub("http.*","",  df$text) # Elimina cualquier tipo de enlace
+  
+  #df$text <- gsub("https://t.co/[A-Za-z\\d]+|&amp;","", df$text)
+  #df$text <- gsub("https://t.co/*","",df$text)
+  #df$text = gsub("http\\w+", "", df$text)
+  
+  df$text <- gsub("[[:digit:]]","" ,df$text)
+
+  #df$text= gsub("[[:digit:]]", "", df$text)
+  #sp2$text  <- gsub("[[:punct:]]", "", sp2$text) #Remueve estos simbolos "a1~!@#$%^&*(){}_+:\"<>?,./;'[]-="
+  #sp2$text <- sp2$text %>% iconv( from = 'UTF-8', to = 'ASCII//TRANSLIT') #Quita tildes pero elimina muchos tweets que empiezan con emoticono
+  
+  df$text= gsub("@\\w+", "", df$text)
+  
+  return(df)
+  
+}
+
+make_url_html <- function(url) {
+  if(length(url) < 2) {
+    if(!is.na(url)) {
+      as.character(glue("<a title = {url} target = '_new' href = '{url}'>{url}</a>") )
+    } else {
+      ""
+    }
+  } else {
+    paste0(purrr::map_chr(url, ~ paste0("<a title = '", .x, "' target = '_new' href = '", .x, "'>", .x, "</a>", collapse = ", ")), collapse = ", ")
+  }
+}
+
+custom_stop_words <- bind_rows(stop_words,
+                               data_frame(word = tm::stopwords("spanish"),
+                                          lexicon = "custom"))
---
+++ dashboard 3_6.R
@@ -0,0 +1,261 @@
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(rtweet)
+library(tidytext)
+library(RColorBrewer)
+library(wordcloud)
+library(wordcloud2)
+library(reactable)
+library(glue)
+library(tm)
+library(plotly)
+library(stm)
+library(quanteda)
+
+token <- authentication()
+
+save.image(file="appshiny.RData")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Tweet Miner"),
+  dashboardSidebar( sidebarMenu(
+    menuItem("Wordcloud", tabName = "wordcloud", icon = icon("dashboard")),
+    menuItem("Tweets", tabName = "widgets", icon = icon("th")),
+    menuItem("Sentimiento",tabName="Sentimiento", icon=icon("f5c0")),
+    menuItem("Topic Modelling",tabName="TopicModel")
+    
+  )),
+  
+  dashboardBody(
+    
+    tabItems(
+      # First tab content
+      tabItem(tabName = "wordcloud",
+              # Boxes need to be put in a row (or column)
+              fluidRow(
+                
+                #??? wordcloud2Output('plot1'),    #, step = NA, width = NULL)
+                
+                
+                box(
+                  title=" Wordcloud",
+                  wordcloud2Output('wordcloud2'),width="100%")
+                #box(plotOutput("plot1", height = 450)),
+                
+              ),  
+              fluidRow(    
+                box(textInput("searchTerm", "Enter data to be searched with '#'", ""),
+                    sliderInput("maxTweets","Number of recent tweets to use for analysis:",min=5,max=10000,value=500), 
+                    submitButton(text="Analyse")),
+                   # vecFun()),
+                
+                box(
+                  title=" Escoge entre 1 palabra o 2",
+                  numericInput("inputnum","Numero de Palabras", 1, min = 1, max = 2),
+                  numericInput("size","Tamaño de la nube", 1, min = 1, max = 20),
+                  selectInput("lan","Language",c("en","es"))
+                  
+                  #title = "Controls",
+                  #sliderInput("slider", "Number of words:", value= 1,min= 1,max= 2)
+                )
+              )
+              
+      ),
+      
+      tabItem(tabName = "widgets", tabsetPanel( tabPanel("User", fluidRow(    
+        
+        box(textInput("searchUser","Enter screen name of user",""),
+            sliderInput("maxTweetsUser","Number of recent tweets to use for analysis:",min=5,max=3200,value=500), 
+            submitButton(text="Analyse")
+        ))
+        ,fluidRow(
+          reactableOutput("tweet_table"))),
+        tabPanel("Topic", fluidRow(    
+          
+          box(textInput("searchTerm2","Enter screen name of user",""),
+              sliderInput("maxTweets2","Number of recent tweets to use for analysis:",min=5,max=10000,value=500), 
+              submitButton(text="Analyse")
+          ))
+          ,fluidRow(
+            reactableOutput("tweet_table2")))
+        
+      )),
+      
+      tabItem(tabName = "Sentimiento",fluidRow( box(textInput("searchsent", "Enter query to be searched with", ""),
+                                                    sliderInput("maxTweetsent","Number of recent tweets to use for analysis:",min=5,max=10000,value=500), 
+                                                    selectInput("RT","Incluir RT",c(TRUE,FALSE)),
+                                                    submitButton(text="Analyse"))),
+              fluidRow(
+        tabBox(
+            width = 12, plotlyOutput("pie", height = "250px") )
+              
+            ),
+        fluidRow(plotlyOutput("ts"))
+        ), #Parantesis que cierra el tabItem
+      
+     
+     tabItem(tabName="TopicModel",  tabsetPanel( tabPanel( "Query",
+             fluidRow(box(title=" '\" Phrase\"' To search for a whole Phrase",
+                          textInput("topic1","Enter Topic 1",""),
+                          textInput("topic2","Enter topic 2"),
+                          sliderInput("maxtopic","Enter number of topics:",min=1,max=4,value=1), 
+                          sliderInput("maxtweettopic","Enter the number of tweets:",min=1,max=10000,value=4000),
+                          numericInput("ngram","Chosse the n-gram",min=1,max=2,value=1),
+                          submitButton(text="Analyse")
+                          )
+                    
+                      ),
+             plotOutput("LDA")
+             
+             ),
+             
+             tabPanel("User",
+              fluidRow(box(textInput("user1","Enter Topic 1",""),
+              textInput("user2","Enter topic 2"),
+              sliderInput("maxtopic2","Enter number of topics:",min=1,max=4,value=1), 
+              sliderInput("maxtuser","Enter the number of tweets:",min=1,max=3200,value=1000),
+              submitButton(text="Analyse")
+                      )
+                      
+                      ),
+                     
+               plotOutput("LDA2")                     
+                      
+                      
+                      
+                      )
+             
+      )
+     
+     ) #El que cierra el tabItem
+    )
+    
+  )
+  
+)
+
+server <- function(input, output,session) {  
+  
+  
+  twtList<-reactive({twtList<-search_tweets(input$searchTerm,n=input$maxTweets,lang=input$lan) })
+  twtList2 <- reactive ({twtList2 <- search_tweets(input$searchTerm2,n=input$maxTweets2)})
+  twtList3 <- reactive ({ get_timeline(input$searchUser,n=input$maxTweetsUser)})
+  twtopic <- reactive ({search_tweets(input$topic1,n=input$maxtweettopic)})
+  twtopic2 <- reactive ({search_tweets(input$topic2,n=input$maxtweettopic)  })
+  twtopicusers <- reactive ({get_timeline(c(input$user1,input$user2),n=input$maxtuser)})
+  
+  twsent <- reactive ({search_tweets(input$searchsent,n=input$maxTweetsent,lang="en")})
+  
+  twtopicut <- reactive ({bind_rows(req(twtopic()),req(twtopic2()))})
+  #datal <-reactive({ funwordcloudsh(req(twtList()),input$inputnum,input$inputfrec,input$inputmax) })  
+  
+  output$wordcloud2 <- renderWordcloud2({
+    
+    funwordcloud2(req(twtList()), input$inputnum,input$size)
+  }) 
+  number <- reactive(nrow(req(twtList)))
+  vecFun <- renderPrint({print(paste0( "Número de tweets analizados= ",number() ))})
+  
+  tweet_table_data <- reactive({
+    req(twtList2())
+    twtList2() %>%
+      select(user_id, status_id, created_at, screen_name, text, favorite_count, retweet_count, urls_expanded_url) %>%
+      mutate(
+        Tweet = glue::glue("{text} <a href='https://twitter.com/{screen_name}/status/{status_id}'>>> </a>"),
+        URLs = purrr::map_chr(urls_expanded_url, make_url_html)
+      )%>%
+      select(DateTime = created_at, User = screen_name, Tweet, Likes = favorite_count, RTs = retweet_count, URLs)
+  })
+  
+  tweet_table_data2 <- reactive({
+    req(twtList3())
+    twtList3() %>%
+      select(user_id, status_id, created_at, screen_name, text, favorite_count, retweet_count, urls_expanded_url) %>%
+      mutate(
+        Tweet = glue::glue("{text} <a href='https://twitter.com/{screen_name}/status/{status_id}'>>> </a>"),
+        URLs = purrr::map_chr(urls_expanded_url, make_url_html)
+      )%>%
+      select(DateTime = created_at, User = screen_name, Tweet, Likes = favorite_count, RTs = retweet_count, URLs)
+  })
+  
+  output$tweet_table <- renderReactable({
+    reactable::reactable(tweet_table_data2(), 
+                         filterable = TRUE, searchable = TRUE, bordered = TRUE, striped = TRUE, highlight = TRUE,
+                         showSortable = TRUE, defaultSortOrder = "desc", defaultPageSize = 25, showPageSizeOptions = TRUE, pageSizeOptions = c(25, 50, 75, 100, 200), 
+                         columns = list(
+                           DateTime = colDef(defaultSortOrder = "asc"),
+                           User = colDef(defaultSortOrder = "asc"),
+                           Tweet = colDef(html = TRUE, minWidth = 190, resizable = TRUE),
+                           Likes = colDef(filterable = FALSE, format = colFormat(separators = TRUE)),
+                           RTs = colDef(filterable =  FALSE, format = colFormat(separators = TRUE)),
+                           URLs = colDef(html = TRUE)
+                         )
+    )
+  })
+  
+  output$tweet_table2 <- renderReactable({
+    reactable::reactable(tweet_table_data(), 
+                         filterable = TRUE, searchable = TRUE, bordered = TRUE, striped = TRUE, highlight = TRUE,
+                         showSortable = TRUE, defaultSortOrder = "desc", defaultPageSize = 25, showPageSizeOptions = TRUE, pageSizeOptions = c(25, 50, 75, 100, 200), 
+                         columns = list(
+                           DateTime = colDef(defaultSortOrder = "asc"),
+                           User = colDef(defaultSortOrder = "asc"),
+                           Tweet = colDef(html = TRUE, minWidth = 190, resizable = TRUE),
+                           Likes = colDef(filterable = FALSE, format = colFormat(separators = TRUE)),
+                           RTs = colDef(filterable =  FALSE, format = colFormat(separators = TRUE)),
+                           URLs = colDef(html = TRUE)
+                         )
+    )
+  })
+  
+  #sentimentplot(twsent())
+ # fig <- reactive({sentimentplot(req(twsent()))})
+  #fig2 <- reactive ({sentimentpie(req(twsent()))})
+  #fig2 <- reactive({sentimentplot(twsent())})
+  output$texto <- renderText("Hola")
+  
+  output$pie <- renderPlotly({fig2<-sentimentpie(req(twsent()))})
+ 
+ 
+  
+  output$ts <- renderPlotly({fig<-sentimentplot(req(twsent()))})
+  
+  output$LDA <- renderPlot({TopicModeler2(twtopicut(),input$maxtopic,input$ngram)})
+  
+  output$LDA2 <- renderPlot({TopicModeler(twtopicusers(),input$maxtopic2)})
+  
+  #output$plot <- renderPlotly({plot_ly(data = pruebaspread, x=~año_dia, y=~seidor, name = 'trace 0', type = 'scatter', mode = 'lines')})
+  
+}
+shinyApp(ui, server)      
+
+
+make_url_html <- function(url) {
+  if(length(url) < 2) {
+    if(!is.na(url)) {
+      as.character(glue("<a title = {url} target = '_new' href = '{url}'>{url}</a>") )
+    } else {
+      ""
+    }
+  } else {
+    paste0(purrr::map_chr(url, ~ paste0("<a title = '", .x, "' target = '_new' href = '", .x, "'>", .x, "</a>", collapse = ", ")), collapse = ", ")
+  }
+}
+
+custom_stop_words <- bind_rows(stop_words,
+                               data_frame(word = tm::stopwords("spanish"),
+                                          lexicon = "custom"))
+
+tabItem(tabName = "Topic Model", plotOutput("LDA")  )
+
+
+prueba <- search_tweets("\"Elon Musk\" OR  Covid19",n=1000)
+
+ds <- Map(
+  "search_tweets",
+  c("\"data science\"", "rstats OR python"),
+  n = 1000
+)
+
+TopicModeler2(extremadura,3,2)
